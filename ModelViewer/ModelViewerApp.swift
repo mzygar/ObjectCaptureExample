d3b1b48c912731db4468cc1ca42c87ebaffe201c
Michal Zygar <mzygar@protonmail.com>
2022-12-14 14:13:30 +0100

    Initial Commit

---
+++ ModelViewer/ModelViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelViewerApp.swift
+//  ModelViewer
+//
+//  Created by Michal Zygar on 14/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct ModelViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
